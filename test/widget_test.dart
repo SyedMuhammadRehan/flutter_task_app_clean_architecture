b1f29c049855c335c42f2d1ee4a8c1a4db53c573
Rehan <syedrehan55.rs@gmail.com>
2024-04-04 10:48:23 +0500

    Update widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,83 +1,51 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_task_app/feature/Todo/data/data%20source/local_database/todo_local_data_source.dart';
-// import 'package:flutter_task_app/feature/Todo/data/repository/todo_repo_impl.dart';
-// import 'package:path_provider/path_provider.dart' as path_provider;
-// import 'package:flutter_task_app/feature/Todo/data/model/todo_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_task_app/feature/Todo/data/data%20source/local_database/todo_local_data_source.dart';
+import 'package:flutter_task_app/feature/Todo/data/repository/todo_repo_impl.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:flutter_task_app/feature/Todo/data/model/todo_model.dart';
 
-// import 'package:dartz/dartz.dart';
-// import 'package:flutter_task_app/feature/Todo/domain/entity/todo_entity.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
-// import 'package:mockito/mockito.dart';
+import 'package:dartz/dartz.dart';
 
-// // Mock TodoDataSource for testing
-// class MockTodoDataSource extends Mock implements TodoDataSource {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:mockito/mockito.dart';
 
-// void main() {
-//   group('TodoRepositoryImpl tests', () {
-//     late TodoRepositoryImpl todoRepository;
-//     late TodoDataSource mockDataSource;
+// Mock TodoDataSource for testing
+class MockTodoDataSource extends Mock implements TodoDataSource {}
 
-//     setUp(() async {
-//       WidgetsFlutterBinding.ensureInitialized();
-//       final appDocumentDir =
-//           await path_provider.getApplicationDocumentsDirectory();
-//       Hive.init(appDocumentDir.path);
-//       Hive.registerAdapter(TodoModelAdapter());
-//       // Open Hive boxes
-//       await Hive.openBox<TodoModel>('todos');
-//        when(mockPathProvider.getApplicationDocumentsDirectory())
-//       .thenAnswer((_) async => Directory('test/app_dir')); 
+void main() {
+  group('TodoRepositoryImpl tests', () {
+    late TodoRepositoryImpl todoRepository;
+    late TodoDataSource mockDataSource;
 
-//   // Create MockTodoDataSource with the mocked path_provider
-//   mockDataSource = MockTodoDataSource(mockPathProvider);
-//   todoRepository = TodoRepositoryImpl(mockDataSource);
-//       mockDataSource = MockTodoDataSource();
-//       todoRepository = TodoRepositoryImpl(mockDataSource);
-//     });
+    setUp(() async {
+      WidgetsFlutterBinding.ensureInitialized();
+      final appDocumentDir =
+          await path_provider.getApplicationDocumentsDirectory();
+      Hive.init(appDocumentDir.path);
+      Hive.registerAdapter(TodoModelAdapter());
+      // Open Hive boxes
+      await Hive.openBox<TodoModel>('todos');
 
-//     test('addTodo should add todo successfully', () async {
-//       // Arrange
-//       const todoText = 'Test todo';
-//       final todoModel = TodoModel(text: todoText);
+      // Create MockTodoDataSource with the mocked path_provider
 
-//       // Stub the addTodo method of mockDataSource to return a Right value
-//       when(mockDataSource.addTodo(todoModel)).thenAnswer((_) async {});
+      mockDataSource = MockTodoDataSource();
+      todoRepository = TodoRepositoryImpl(mockDataSource);
+    });
 
-//       // Act
-//       final result = await todoRepository.addTodo(text: todoText);
+    test('addTodo should add todo successfully', () async {
+      // Arrange
+      const todoText = 'Test todo';
+      final todoModel = TodoModel(text: todoText);
 
-//       // Assert
-//       expect(result, const Right(null));
-//     });
+      // Stub the addTodo method of mockDataSource to return a Right value
+      when(mockDataSource.addTodo(todoModel)).thenAnswer((_) async {});
 
-//     test('deleteTodo should delete todo successfully', () async {
-//       // Arrange
-//       const todoId = 1;
+      // Act
+      final result = await todoRepository.addTodo(text: todoText);
 
-//       // Stub the deleteTodo method of mockDataSource to return a Right value
-//       when(mockDataSource.deleteTodo(todoId)).thenAnswer((_) async {});
-
-//       // Act
-//       final result = await todoRepository.deleteTodo(id: todoId);
-
-//       // Assert
-//       expect(result, const Right(null));
-//     });
-
-//     test('getallTodo should return list of todos successfully', () async {
-//       // Arrange
-//       final todoModels = [TodoModel(text: 'Todo 1'), TodoModel(text: 'Todo 2')];
-
-//       // Stub the getAllTodos method of mockDataSource to return a list of todo models
-//       when(mockDataSource.getAllTodos()).thenAnswer((_) async => todoModels);
-
-//       // Act
-//       final result = await todoRepository.getallTodo();
-
-//       // Assert
-//       expect(result,
-//           Right(todoModels.map((model) => Todo(text: model.text)).toList()));
-//     });
-//   });
-// }
+      // Assert
+      expect(result, const Right(null));
+    });
+  });
+}
